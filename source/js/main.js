899e37c01e329f7baac3f23ec81dba107b12201a
David Babbitt <davidb@spiceworks.com>
2016-01-05 17:35:51 -0600

    adding datatables

--- source/js/main.js
+++ source/js/main.js
@@ -2,4 +2,9 @@
  *= require jquery
  *= require underscore
  *= require bootstrap
+ *= require datatables.net/js/jquery.dataTables
+ *= require datatables.net-bs/js/dataTables.bootstrap
+ *= require datatables.net-buttons/js/dataTables.buttons
+ *= require datatables.net-buttons-bs/js/buttons.bootstrap
+ *= require datatables.net-select/js/dataTables.select
  */
